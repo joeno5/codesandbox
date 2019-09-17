eed0b57ec328c1a99322428bf416c0f807ec67de
joeno5 <31912002+joeno5@users.noreply.github.com>
2019-09-17 10:22:36 +0800

    again

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import LoginForm from "./login";
+import MyMenu from "./menu";
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Login</h1>
+      <div className="menu">
+        <MyMenu />
+      </div>
       <div>
         <LoginForm />
       </div>
